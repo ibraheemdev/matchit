387f469e4ff5e694b06ee05eadeab7307ce487ed
Ibraheem <ibrah1440@gmail.com>
2020-10-24 23:11:12 -0400

    convenience `Request` and `Response` types

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
 #![deny(clippy::all)]
+#![forbid(unsafe_code)]
 
+pub mod app;
+pub mod handler;
+pub mod request;
+pub mod response;
+pub mod route;
 pub mod router;
 pub mod tree;
+
+#[macro_use]
+extern crate log;
+
+
+pub(crate) type Request = hyper::Request<hyper::Body>;
+pub(crate) type Response = hyper::Response<hyper::Body>;
