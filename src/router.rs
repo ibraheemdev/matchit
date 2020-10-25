f9c8a4c662b0bcbe10200b3725aac06a77ef207e
Ibraheem <ibrah1440@gmail.com>
2020-10-24 23:09:11 -0400

    update router to use http::Method

--- src/router.rs
+++ src/router.rs
@@ -1,21 +1,19 @@
+//! The router relies on a radix tree structure which makes heavy use of *common prefixes*
+//! see [tree.rs](../tree/index.html) for more details
 use crate::tree::{Node, Params};
-use futures::future::Future;
-use hyper::{Body, Response};
-use std::collections::BTreeMap;
-use std::pin::Pin;
-
-pub type BoxFut = Pin<Box<dyn Future<Output = Result<Response<Body>, hyper::Error>>>>;
+use http::Method;
+use std::collections::HashMap;
 
 /// Router is container which can be used to dispatch requests to different
 /// handler functions via configurable routes
 pub struct Router<T> {
-  pub trees: BTreeMap<String, Node<T>>,
+  pub methods: HashMap<Method, Node<T>>,
 }
 
 impl<T> Default for Router<T> {
   fn default() -> Self {
     Router {
-      trees: BTreeMap::new(),
+      methods: HashMap::with_capacity(7),
     }
   }
 }
@@ -23,50 +21,50 @@ impl<T> Default for Router<T> {
 impl<T> Router<T> {
   /// add a handler for http method GET
   pub fn get(&mut self, path: &str, handle: T) {
-    self.handle("GET", path, handle);
+    self.handle(Method::GET, path, handle);
   }
 
   /// add a handler for http method HEAD
   pub fn head(&mut self, path: &str, handle: T) {
-    self.handle("HEAD", path, handle);
+    self.handle(Method::HEAD, path, handle);
   }
 
   /// add a handler for http method OPTIONS
   pub fn options(&mut self, path: &str, handle: T) {
-    self.handle("OPTIONS", path, handle);
+    self.handle(Method::OPTIONS, path, handle);
   }
 
   /// add a handler for http method POST
   pub fn post(&mut self, path: &str, handle: T) {
-    self.handle("POST", path, handle);
+    self.handle(Method::POST, path, handle);
   }
 
   /// add a handler for http method PUT
   pub fn put(&mut self, path: &str, handle: T) {
-    self.handle("PUT", path, handle);
+    self.handle(Method::PUT, path, handle);
   }
 
   /// add a handler for http method PATCH
   pub fn patch(&mut self, path: &str, handle: T) {
-    self.handle("PATCH", path, handle);
+    self.handle(Method::PATCH, path, handle);
   }
 
   /// add a handler for http method DELTE
   pub fn delete(&mut self, path: &str, handle: T) {
-    self.handle("DELETE", path, handle);
+    self.handle(Method::DELETE, path, handle);
   }
 
   /// Handle registers a new request handle with the given path and method.
   /// For GET, POST, PUT, PATCH and DELETE requests, the respective shortcut
   /// functions can be used.
-  pub fn handle(&mut self, method: &str, path: &str, handle: T) {
+  pub fn handle(&mut self, method: Method, path: &str, handle: T) {
     if !path.starts_with('/') {
       panic!("path must begin with '/' in path '{}'", path);
     }
 
     self
-      .trees
-      .entry(method.to_string())
+      .methods
+      .entry(method)
       .or_insert_with(Node::default)
       .add_route(path, handle);
   }
@@ -75,9 +73,9 @@ impl<T> Router<T> {
   /// Returns the handle function and the path parameter values if the path is found.
   /// Otherwise, the third return value indicates whether a redirection to
   /// the same path without the trailing slash should be performed.
-  pub fn lookup(&mut self, method: &str, path: &str) -> (Option<&T>, Params, bool) {
+  pub fn lookup(&mut self, method: &Method, path: &str) -> (Option<&T>, Params, bool) {
     self
-      .trees
+      .methods
       .get_mut(method)
       .map(|n| n.get_value(path))
       .unwrap_or((None, Params::default(), false))
@@ -85,27 +83,25 @@ impl<T> Router<T> {
 
   /// returns a list of the allowed methods for a specific path
   /// eg: 'GET, PATCH, OPTIONS'
-  pub fn allowed(&self, path: &str, req_method: &str) -> String {
-    let mut allowed: Vec<String> = Vec::new();
+  pub fn allowed(&self, path: &str, req_method: Method) -> String {
+    let mut allowed: Vec<Method> = Vec::new();
     match path {
       "*" => {
-        for method in self.trees.keys() {
-          if method != "OPTIONS" {
-            allowed.push(method.to_string());
+        for method in self.methods.keys() {
+          if method != Method::OPTIONS {
+            allowed.push(method.clone());
           }
         }
       }
       _ => {
-        for method in self.trees.keys() {
-          if method == req_method || method == "OPTIONS" {
+        for method in self.methods.keys() {
+          if method == req_method || method == Method::OPTIONS {
             continue;
           }
-
-          if let Some(tree) = self.trees.get(method) {
+          if let Some(tree) = self.methods.get(&method.clone()) {
             let (handle, _, _) = tree.get_value(path);
-
             if handle.is_some() {
-              allowed.push(method.to_string());
+              allowed.push(method.clone());
             }
           };
         }
@@ -113,10 +109,10 @@ impl<T> Router<T> {
     };
 
     if !allowed.is_empty() {
-      allowed.push(String::from("OPTIONS"))
+      allowed.push(Method::OPTIONS)
     }
 
-    allowed.join(", ")
+    allowed.iter().map(|method| method.to_string()).collect()
   }
 }
 
@@ -126,14 +122,14 @@ mod tests {
   #[should_panic(expected = "path must begin with '/' in path 'invalid'")]
   fn handle_invalid_path() {
     use crate::router::{Params, Router};
-    use hyper::{Body, Request, Response};
+    use hyper::{Body, Method, Response};
 
     let mut router = Router::default();
 
     router.handle(
-      "GET",
+      Method::GET,
       "invalid",
-      |_req: Request<Body>, _: Params| -> Response<Body> { Response::new(Body::from("test")) },
+      |_req: crate::Request, _: Params| -> crate::Response { Response::new(Body::from("test")) },
     );
   }
 }
