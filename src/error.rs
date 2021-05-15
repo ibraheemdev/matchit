2876b5446e14ce4d9db308a8320f28d568833661
ibraheemdev <ibrah1440@gmail.com>
2021-05-14 21:03:00 -0400

    split up lib.rs

---
+++ src/error.rs
@@ -0,0 +1,114 @@
+use std::fmt;
+use std::str;
+
+/// Represents errors that can occur when inserting a new route.
+#[non_exhaustive]
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum InsertError {
+    /// The inserted path conflicts with an existing route.
+    ///
+    /// This error may unexpectedly come up when registering routes like the following:
+    ///
+    /// ```text
+    /// /user/:id
+    /// /user/get
+    /// ```
+    ///
+    /// This is due to the strict requirements of the internal radix tree,
+    /// and should be fixed in the future.
+    Conflict {
+        /// The existing route that the insertion is conflicting with.
+        with: String,
+    },
+    /// Only one parameter per path segment is allowed.
+    TooManyParams,
+    /// Parameters must be registered with a name.
+    UnnamedParam,
+    /// Catch-all parameters are only allowed at the end of a path.
+    InvalidCatchAll,
+}
+
+impl fmt::Display for InsertError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Conflict { with } => {
+                write!(
+                    f,
+                    "insertion failed due to conflict with previously registered path: {}",
+                    with
+                )
+            }
+            Self::TooManyParams => write!(f, "only one parameter is allowed per path segment"),
+            Self::UnnamedParam => write!(f, "parameters must be registered with a name"),
+            Self::InvalidCatchAll => write!(
+                f,
+                "catch-all parameters are only allowed at the end of a path"
+            ),
+        }
+    }
+}
+
+impl std::error::Error for InsertError {}
+
+impl InsertError {
+    pub(crate) fn conflict(path: &str, prefix: &[u8], route: &[u8]) -> Self {
+        let with = format!(
+            "{}{}",
+            &path[..path.rfind(str::from_utf8(prefix).unwrap()).unwrap()],
+            str::from_utf8(route).unwrap(),
+        );
+
+        InsertError::Conflict { with }
+    }
+}
+
+/// A failed match attempt, with trailing slash redirect information.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct MatchError {
+    tsr: bool,
+}
+
+impl MatchError {
+    /// Indicates whether a route exists at the same path with/without a trailing slash.
+    /// ```rust
+    /// # use matchit::Node;
+    ///
+    /// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// let mut matcher = Node::new();
+    /// matcher.insert("/home", "Welcome!")?;
+    /// matcher.insert("/blog/", "Our blog.")?;
+    ///
+    /// // a route exists without the trailing slash
+    /// if let Err(err) = matcher.at("/home/") {
+    ///     assert!(err.tsr());
+    /// }
+    ///
+    /// // a route exists with a trailing slash
+    /// if let Err(err) = matcher.at("/blog") {
+    ///     assert!(err.tsr());
+    /// }
+    ///
+    /// // no routes match, with or without a trailing slash
+    /// if let Err(err) = matcher.at("/foobar") {
+    ///     assert!(!err.tsr());
+    /// }
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn tsr(self) -> bool {
+        self.tsr
+    }
+
+    /// Creates a new match error with the given trailing slash redirect.
+    pub fn new(tsr: bool) -> Self {
+        Self { tsr }
+    }
+}
+
+impl fmt::Display for MatchError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "no route registered under the given path")
+    }
+}
+
+impl std::error::Error for MatchError {}
---
+++ src/params.rs
@@ -0,0 +1,221 @@
+use std::iter::{self, FromIterator};
+use std::mem;
+use std::slice;
+
+/// A single URL parameter, consisting of a key and a value.
+#[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Default)]
+struct Param<'k, 'v> {
+    key: &'k str,
+    value: &'v str,
+}
+
+/// A list of parameters returned by a route match.
+///
+/// ```rust
+/// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+/// # let mut matcher = matchit::Node::new();
+/// # matcher.insert("/users/:id", true).unwrap();
+/// let matched = matcher.at("/users/1")?;
+///
+/// // you can iterate through the keys and values
+/// for (key, value) in matched.params.iter() {
+///     println!("key: {}, value: {}", key, value);
+/// }
+///
+/// // or get a specific value by key
+/// let id = matched.params.get("id");
+/// assert_eq!(id, Some("1"));
+/// # Ok(())
+/// # }
+/// ```
+#[derive(Debug, PartialEq, Eq, Ord, PartialOrd)]
+pub struct Params<'k, 'v> {
+    kind: ParamsKind<'k, 'v>,
+}
+
+// most routes have 1/2 parameters, so we can avoid a heap allocation in that case.
+const SMALL: usize = 2;
+
+#[derive(Debug, PartialEq, Eq, Ord, PartialOrd)]
+enum ParamsKind<'k, 'v> {
+    Small([Param<'k, 'v>; SMALL], usize),
+    Large(Vec<Param<'k, 'v>>),
+}
+
+impl<'k, 'v> Default for Params<'k, 'v> {
+    fn default() -> Self {
+        let kind = ParamsKind::Small(Default::default(), 0);
+        Self { kind }
+    }
+}
+
+impl<'k, 'v> Params<'k, 'v> {
+    /// Creates a new list of URL parameters.
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Returns the value of the first parameter registered matched for the given key.
+    pub fn get(&self, key: impl AsRef<str>) -> Option<&'v str> {
+        match &self.kind {
+            ParamsKind::Small(arr, _) => arr
+                .iter()
+                .find(|param| param.key == key.as_ref())
+                .map(|param| param.value),
+            ParamsKind::Large(vec) => vec
+                .iter()
+                .find(|param| param.key == key.as_ref())
+                .map(|param| param.value),
+        }
+    }
+
+    /// Returns an iterator over the parameters in the list.
+    pub fn iter(&self) -> Iter<'_, 'k, 'v> {
+        Iter::new(self)
+    }
+
+    /// Returns `true` if there are no parameters in the list.
+    pub fn is_empty(&self) -> bool {
+        match &self.kind {
+            ParamsKind::Small(_, len) => *len == 0,
+            ParamsKind::Large(vec) => vec.is_empty(),
+        }
+    }
+
+    /// Inserts a key value parameter pair into the list.
+    pub fn push(&mut self, key: &'k str, value: &'v str) {
+        #[cold]
+        fn drain_to_vec<T: Default>(len: usize, elem: T, arr: &mut [T; SMALL]) -> Vec<T> {
+            let mut vec = Vec::with_capacity(len + 1);
+            vec.extend(arr.iter_mut().map(mem::take));
+            vec.push(elem);
+            return vec;
+        }
+
+        let param = Param { key, value };
+        match &mut self.kind {
+            ParamsKind::Small(arr, len) => {
+                if *len == SMALL {
+                    self.kind = ParamsKind::Large(drain_to_vec(*len, param, arr));
+                    return;
+                }
+                arr[*len] = param;
+                *len += 1;
+            }
+            ParamsKind::Large(vec) => vec.push(param),
+        }
+    }
+}
+
+impl<'k, 'v> FromIterator<(&'k str, &'v str)> for Params<'k, 'v> {
+    fn from_iter<T: IntoIterator<Item = (&'k str, &'v str)>>(iter: T) -> Self {
+        let mut params = Self::default();
+        for (key, value) in iter.into_iter() {
+            params.push(key, value);
+        }
+        params
+    }
+}
+
+impl<'k, 'v> IntoIterator for Params<'k, 'v> {
+    type IntoIter = IntoIter<'k, 'v>;
+    type Item = (&'k str, &'v str);
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter::new(self)
+    }
+}
+
+/// An iterator over the keys and values of URL [parameters](crate::Params).
+pub struct Iter<'ps, 'k, 'v> {
+    kind: IterKind<'ps, 'k, 'v>,
+}
+
+impl<'ps, 'k, 'v> Iter<'ps, 'k, 'v> {
+    fn new(params: &'ps Params<'k, 'v>) -> Self {
+        let kind = match &params.kind {
+            ParamsKind::Small(arr, len) => IterKind::Small(arr.iter().take(*len)),
+            ParamsKind::Large(vec) => IterKind::Large(vec.iter()),
+        };
+        Self { kind }
+    }
+}
+
+enum IterKind<'ps, 'k, 'v> {
+    Small(iter::Take<slice::Iter<'ps, Param<'k, 'v>>>),
+    Large(slice::Iter<'ps, Param<'k, 'v>>),
+}
+
+impl<'ps, 'k, 'v> Iterator for Iter<'ps, 'k, 'v> {
+    type Item = (&'k str, &'v str);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.kind {
+            IterKind::Small(ref mut iter) => iter.next().map(|p| (p.key, p.value)),
+            IterKind::Large(ref mut iter) => iter.next().map(|p| (p.key, p.value)),
+        }
+    }
+}
+
+/// An owned iterator over the keys and values of URL [parameters](crate::Params).
+pub struct IntoIter<'k, 'v> {
+    kind: IntoIterKind<'k, 'v>,
+}
+
+impl<'k, 'v> IntoIter<'k, 'v> {
+    fn new(params: Params<'k, 'v>) -> Self {
+        let kind = match params.kind {
+            ParamsKind::Small(arr, len) => {
+                IntoIterKind::Small(std::array::IntoIter::new(arr).take(len))
+            }
+            ParamsKind::Large(vec) => IntoIterKind::Large(vec.into_iter()),
+        };
+        Self { kind }
+    }
+}
+
+enum IntoIterKind<'k, 'v> {
+    Small(iter::Take<std::array::IntoIter<Param<'k, 'v>, SMALL>>),
+    Large(std::vec::IntoIter<Param<'k, 'v>>),
+}
+
+impl<'k, 'v> Iterator for IntoIter<'k, 'v> {
+    type Item = (&'k str, &'v str);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.kind {
+            IntoIterKind::Small(ref mut iter) => iter.next().map(|p| (p.key, p.value)),
+            IntoIterKind::Large(ref mut iter) => iter.next().map(|p| (p.key, p.value)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn params() {
+        let vec = vec![
+            ("hello", "hello"),
+            ("world", "world"),
+            ("foo", "foo"),
+            ("bar", "bar"),
+            ("baz", "baz"),
+        ];
+
+        let params = vec.iter().cloned().collect::<Params<'_, '_>>();
+        for (key, value) in vec.clone() {
+            assert_eq!(params.get(key), Some(value));
+        }
+
+        let mut params = Params::default();
+        for (key, value) in vec.clone() {
+            params.push(key, value);
+            assert_eq!(params.get(key), Some(value));
+        }
+
+        assert!(params.iter().eq(vec.clone()));
+        assert!(params.into_iter().eq(vec.clone()));
+    }
+}
