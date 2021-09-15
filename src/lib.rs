157c6a44459a6bc83c3e95e7abfe48226882708a
Ibraheem Ahmed <ibrah1440@gmail.com>
2021-09-15 07:31:53 -0400

    release 0.4.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(rust_2018_idioms)]
 //! # MatchIt
 //!
-//! [![Documentation](https://img.shields.io/badge/docs-0.4.2-4d76ae?style=for-the-badge)](https://docs.rs/matchit)
+//! [![Documentation](https://img.shields.io/badge/docs-0.4.3-4d76ae?style=for-the-badge)](https://docs.rs/matchit)
 //! [![Version](https://img.shields.io/crates/v/matchit?style=for-the-badge)](https://crates.io/crates/matchit)
 //! [![License](https://img.shields.io/crates/l/matchit?style=for-the-badge)](https://crates.io/crates/matchit)
 //! [![Actions](https://img.shields.io/github/workflow/status/ibraheemdev/matchit/Rust/master?style=for-the-badge)](https://github.com/ibraheemdev/matchit/actions)
